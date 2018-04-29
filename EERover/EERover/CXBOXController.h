ce8c1f6e3106c0983f707eb94734f06fa89e4c5e
NasimMM <NasimMM@users.noreply.github.com>
2018-04-29 21:13:07 +0100

    EERover Program VS Update

--- EERover/EERover/CXBOXController.h
+++ EERover/EERover/CXBOXController.h
@@ -5,13 +5,9 @@
 // No MFC
 #define WIN32_LEAN_AND_MEAN
 
-// We need the Windows Header and the XInput Header
 #include <windows.h>
 #include <XInput.h>
 
-// Now, the XInput Library
-// NOTE: COMMENT THIS OUT IF YOU ARE NOT USING
-// A COMPILER THAT SUPPORTS THIS METHOD OF LINKING LIBRARIES
 #pragma comment(lib, "XInput.lib")
 
 // XBOX Controller Class Definition
